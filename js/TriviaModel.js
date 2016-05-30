1c861400dbeb080c7ec216bccb73ab5d705bdec7
Uman Shahzad <emidanrko564@hotmail.com>
2016-05-30 16:46:15 -0400

    Adds blank slates for trivia's controller and model

---
+++ js/TriviaModel.js
@@ -0,0 +1,6 @@
+var questions = new Map();
+questions.set("George Washington was the first president of the United States", true)
+questions.set("Hunter College is located in Queens", false)
+questions.set("The abacus was invented by the Incas", false)
+questions.set("The year 2000 bug resulted from an error in the assembly of transistors for IBM computers", false)
+
