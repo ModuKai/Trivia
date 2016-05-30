c5b2945922548fa325a3c58bd1c3776e785b21e1
Uman Shahzad <emidanrko564@hotmail.com>
2016-05-30 17:44:42 -0400

    Adds BeginQuiz button functionality

--- js/Trivia.js
+++ js/Trivia.js
@@ -5,10 +5,17 @@ questions.set("The abacus was invented by the Incas", false)
 questions.set("The year 2000 bug resulted from an error in the assembly of transistors for IBM computers", false)
 
 document.getElementById("begin_quiz_button").onclick = function() {
-	alert("WATUPP");
-}
+	var questionObject = document.getElementById("question");
+	var questionSet    = questions.keys();
+
+	// Remove "Begin Quiz!" button.
+	document.getElementById("begin_quiz_button").style.display = "none"	;
+
+	// Add the question.
+	questionObject.textContent = questionSet.next().value;
+};
 
 document.getElementById("submit_answer_button").onclick = function() {
 	
-}
+};
 
